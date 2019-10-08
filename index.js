ef30c57f8eea5edbf41370628f2276a3f323d13b
Bill G <gbilly3@gmail.com>
2019-10-08 19:43:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
